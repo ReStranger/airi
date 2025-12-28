7cad154aadfda5027a040d1008d840869cc7bc12
ReStranger <restranger@disroot.org>
2025-12-28 15:57:34 +0300

    fix(ci): Fixed sending output to tty
    - We don't have a tty in the github actions runtime
    
    Signed-off-by: ReStranger <restranger@disroot.org>

--- nix/update-pnpm-deps-hash.sh
+++ nix/update-pnpm-deps-hash.sh
@@ -2,8 +2,10 @@
 
 set -eux
 
+ls /dev
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
 :> pnpm-deps-hash.txt # Clear hash to trigger rebuild
 # Redirect stderr to stdout for grep while keeping printing on stderr
-HASH=$(nix build ..#airi.pnpmDeps 2> >(tee /dev/tty) | grep -oP 'got: +\K\S+')
+HASH=$(nix build ..#airi.pnpmDeps 2> >(tee /dev/stderr) | grep -oP 'got: +\K\S+')
 echo -n $HASH > pnpm-deps-hash.txt
